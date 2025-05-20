d1c57ec0c14e1e79ea4bda62e5ccb48f8dc4ebbe
pavvelpotapov <pavelminsk1979@mail.ru>
2025-05-20 20:42:27 +0300

    commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('nothing')
+    const [title, setTitle] = useState('')
+
+    const creatTitle = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const handleOnClick = () => {
+        axios.post('http://localhost:3010', {value,title})
+            .then(res => {
+                console.log(res.data)
+                setValue(res.data);
+            })
+            .catch(error => {
+                console.log(error.message)
+                setValue(error.message);
+            })
+        setTitle('')
+    }
+
+    return (
+        <div className="App">
+            <input
+
+                onChange={creatTitle}
+                value={title}/>
+            <div> ВВЕДИ ТЕКСТ</div>
+
+            <button
+                onClick={handleOnClick}
+            > ЗАПРОС НА БЭК
+            </button>
+            <div>{value}</div>
+        </div>
+
+    );
 }
 
 export default App;
