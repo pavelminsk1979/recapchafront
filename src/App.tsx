f22df1a75ab4b5d0c190b4ff672d98c4063c2689
pavvelpotapov <pavelminsk1979@mail.ru>
2025-05-21 20:12:15 +0300

    commit ок

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
+
+declare global {
+    interface Window {
+        grecaptcha: any;
+    }
+}
 
 function App() {
     const [value, setValue] = useState('nothing')
     const [title, setTitle] = useState('')
 
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://www.google.com/recaptcha/api.js?render=6LcIaUMrAAAAACLiH5fja6dupHb3pF3mMh_UqmqE";
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     const creatTitle = (event: ChangeEvent<HTMLInputElement>) => {
         setTitle(event.currentTarget.value)
+
+
     }
 
+
     const handleOnClick = () => {
-        axios.post('http://localhost:3010', {value,title})
-            .then(res => {
-                console.log(res.data)
-                setValue(res.data);
-            })
-            .catch(error => {
-                console.log(error.message)
-                setValue(error.message);
-            })
-        setTitle('')
+        if (window.grecaptcha) {
+            window.grecaptcha.execute('6LcIaUMrAAAAACLiH5fja6dupHb3pF3mMh_UqmqE', {action: 'submit'})
+                .then((token: any) => {
+                    console.log('token', token)
+                    axios.post('http://localhost:3010', {value, title, token})
+                        .then(res => {
+                            console.log(res.data)
+                            setValue(res.data);
+                        })
+                        .catch(error => {
+                            console.log(error.message)
+                            setValue(error.message);
+                        })
+                    setTitle('')
+                })
+
+        }
+
+
     }
 
     return (
